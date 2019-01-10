7fae54bfd9c887f5e1a8889ffeb1181df3065455
Eoin Nolan <eoin.nolan@intercom.io>
2019-01-10 18:34:26 +0000

    fixup

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'litreka',
     environment,
     rootURL: '/',
-    locationType: 'hash',
+    locationType: 'auto',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
